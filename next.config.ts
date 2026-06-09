005bb16bb7bf56ee722ca46d5758adb3f5e0531c
Ayush Bajpai <bajpai.ayush004@gmail.com>
2026-06-09 18:45:37 +0530

    Fix build error

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
+
   turbopack: {
     root: path.resolve(__dirname),
   },
